79bd1a01de3565ec4c6db359eaf750c3f247e771
visheshsain <visheshsain48@gmail.com>
2021-07-25 09:36:06 +0530

    project43

---
+++ js/Code.js
@@ -0,0 +1,26 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+// add code for displaying rest of the names and the hints.
+    
+    fill("white");
+    textSize(15);
+    text("C U T N I F O N",700,150);
+    fill("lightblue");
+    text("Hint: Perform a pirtecular task",700,170);
+
+    fill("white");
+    textSize(15);
+    text("A T A D A S B A",100,310);
+    fill("lightblue");
+    text("Hint : store all infomation ",100,330);
+
+}
